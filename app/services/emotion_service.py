24e0f592a3bc96c8f033c92f3c33bf931ec344f8
Kimsh <julle03@naver.com>
2025-08-22 17:42:20 +0900

    feat : 모델 위치 변경

--- app/services/emotion_service.py
+++ app/services/emotion_service.py
@@ -2,7 +2,7 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
-MODEL_PATH = "outputs_trainer_final2/best_model"
+MODEL_PATH = "best_model"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_PATH)
 model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH)
